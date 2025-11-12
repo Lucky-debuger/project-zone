36fc7379446bded24b8dd1b641d9c1ae76a81002
Lucky-debuger <79850821+Lucky-debuger@users.noreply.github.com>
2025-11-12 23:00:20 +0300

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] VariableJoystick joystick;
+    [SerializeField] Rigidbody2D _rigidbody;
+    [SerializeField] Canvas inputCanvas;
+    [SerializeField] bool isJoystick;
+
+    public float movementSpeed;
+
+    // Добавляем кэшированные векторы для оптимизации 
+    private Vector2 movementDirection;
+    private Vector2 newVelocity;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        EnableJoystickInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJoystick)
+        {
+            var movementDirection = new Vector2(joystick.Direction.x, joystick.Direction.y);
+            // ✅ Оптимизация: избегаем создания новых Vector2 каждый кадр
+            newVelocity.x = movementDirection.x * movementSpeed;
+            newVelocity.y = movementDirection.y * movementSpeed;
+            _rigidbody.velocity = newVelocity;
+        }
+    }
+
+    private void EnableJoystickInput()
+    {
+        isJoystick = true;
+        inputCanvas.gameObject.SetActive(true);
+    }
+    
+    
+}
