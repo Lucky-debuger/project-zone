5aad7c949313c1a399ad350293e791bfb214da02
unknown <lesha-butenko1@mail.ru>
2025-12-17 12:04:28 +0300

    Fixed enemy move bug

--- Assets/Scripts/NewEnemy/EnemyController.cs
+++ Assets/Scripts/NewEnemy/EnemyController.cs
@@ -8,8 +8,10 @@ public class EnemyController : MonoBehaviour, IDamageable
     [Header("Links")]
     public Transform player;
     [SerializeField] private HealthSystem healthSystem;
+    [SerializeField] private Rigidbody2D rb;
     
     private EnemyState currentState;
+    private Vector2 _smoothVelocity = Vector2.zero;
     // private float currentHealth;
     
     
@@ -17,13 +19,11 @@ public class EnemyController : MonoBehaviour, IDamageable
     {
         // currentHealth = enemyData.health;
         healthSystem.SetHealthPoints(100);
-        
         player = GameObject.FindGameObjectWithTag("Player").transform; // Подумать о вариантах получения объекта
-        
         SetState(new IdleState(this));
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         currentState?.UpdateState();
     }
@@ -40,10 +40,22 @@ public class EnemyController : MonoBehaviour, IDamageable
         return Vector2.Distance(transform.position, player.position) <= enemyData.detectionRange;
     }
     
-    public void MoveTowardsPlayer()
+    public void MoveTowardsPlayer() // Разобраться лучше, как работает
     {
-        Vector2 direction = (player.position - transform.position).normalized;
-        transform.Translate(direction * enemyData.moveSpeed * Time.deltaTime);
+        // Vector2 direction = (player.position - transform.position).normalized;
+        // transform.Translate(direction * enemyData.moveSpeed * Time.deltaTime);
+        Vector2 targetPosition = player.position;
+        Vector2 currentPosition = rb.position;
+
+        Vector2 smoothPosition = Vector2.SmoothDamp(
+            currentPosition,
+            targetPosition,
+            ref _smoothVelocity,
+            0.3f, // Вермя сглаживания
+            enemyData.moveSpeed
+        );
+
+        rb.MovePosition(smoothPosition);
     }
 
     public bool CanAttackPlayer() // Переписал на свой вариант
